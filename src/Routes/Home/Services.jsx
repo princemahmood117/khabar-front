22489442ed5f7492698819072bc1d30eccd39fd1
princemahmood117 <princemahmood117@gmail.com>
2024-11-02 21:03:10 +0600

    color changed

--- src/Routes/Home/Services.jsx
+++ src/Routes/Home/Services.jsx
@@ -1,44 +1,5 @@
 
 
-// import useServices from "../../hooks/useServices";
-// import ServiceCard from "./ServiceCard";
-
-
-// const Services = () => {
-
-//     const services = useServices();
-    
-   
-
-    
-//     return (
-//         <div className="mt-4 p-2">
-
-//             <div className="space-y-2 text-center">
-//                 <h3 className="text-2xl text-orange-600 font-bold">Available Foods</h3>
-              
-//                 <p>Find your desired foods here </p>
-//             </div>
-
-//             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 mt-8 space-y-4">
-//                 {
-//                     services.map(service => <ServiceCard key={service._id} service={service} services={services}></ServiceCard>)
-//                 }
-//             </div>
-            
-//         </div>
-//     );
-// };
-
-// export default Services;
-
-
-
-
-
-
-
-
 import useServices from "../../hooks/useServices";
 import ServiceCard from "./ServiceCard";
 import { useState, useEffect } from "react";
@@ -59,7 +20,7 @@ const Services = () => {
   return (
     <div className="mt-4 p-2">
       <div className="space-y-2 text-center">
-        <h3 className="text-2xl text-orange-600 font-bold">Available Foods</h3>
+        <h3 className="text-2xl text-red-500 font-bold">Available Foods</h3>
         <p>Find your desired foods here</p>
       </div>
 
